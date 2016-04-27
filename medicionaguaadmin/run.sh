2d98b1f91a96ab041215b4d7db81e108983e9cee
Alejandro Bustos <alejandrob@live.cl>
2016-04-27 11:29:05 -0300

    [fix]: Se arregla el uso de root para el container

--- medicionaguaadmin/run.sh
+++ medicionaguaadmin/run.sh
@@ -1,1 +1,1 @@
-docker run -it --net="host" -v /home/alejandro/Projects/job/MedicionAguaAdmin:/app --name="medicionaguaadmin" mt/medicionaguaadmin
+docker run -it --net="host" -v /home/alejandro/Projects/job/MedicionAguaAdmin:/home/dev/app --name="medicionaguaadmin" --privileged mt/medicionaguaadmin
