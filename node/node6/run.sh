33cefeaaaba05953abdf95617f776828474efaec
Alejandro Bustos <alejandrob@live.cl>
2016-08-11 18:06:44 -0400

    Se añade funcionalidad para preguntar al usuario por el nombre del container

--- node/node6/run.sh
+++ node/node6/run.sh
@@ -1,1 +1,3 @@
-docker run -it -p 7006:7006 -e "APP_PORT=7006" --name indicadores-api -v $(pwd):/home/dev/app node6
+echo "Ingrese un nombre para el contenedor:"
+read containername
+docker run -it -p 7006:7006 -e "APP_PORT=7006" --name $containername -v $(pwd):/home/dev/app node6
